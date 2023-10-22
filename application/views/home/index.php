e73d7aef578ee744a19c6a63e2a89541eb2ef0f6
amift <miftahul.dev@gmail.com>
2023-10-22 22:38:08 +0700

    test mobile layout

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -15,8 +15,73 @@
 		<![endif]-->
 	</head>
 	<body>
-		<h1 class="text-center">Hello World</h1>
-		<div id="all_val"></div>
+		<div class="container">
+			<h1 class="text-center">Home Controller</h1>
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-lg-4 p-2">
+						<div class="card">
+						  <div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						  </div>
+						</div>					
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-4 p-2">
+						<div class="card">
+						  <div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						  </div>
+						</div>					
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-4 p-2">
+						<div class="card">
+						  <div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						  </div>
+						</div>					
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-4 p-2">
+						<div class="card">
+						  <div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						  </div>
+						</div>					
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-4 p-2">
+						<div class="card">
+						  <div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						  </div>
+						</div>					
+				</div>
+				<div class="col-xs-12 col-sm-6 col-lg-4 p-2">
+						<div class="card">
+						  <div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						  </div>
+						</div>					
+				</div>
+			</div>
+			<ul>
+				<li>voltage</li>
+				<li>current</li>
+				<li>power</li>
+				<li>energy</li>
+				<li>frequency</li>
+				<li>pf</li>
+			</ul>
+		</div>
 
 		<!-- jQuery -->
 		<script src="<?php echo base_url()?>assets/jquery/jquery-2.2.3.min.js"></script>
